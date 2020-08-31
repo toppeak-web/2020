44e0f52cb1418e64844bc1275cda7817cf539286
toppeak <xkqvor123@gmail.com>
2020-09-01 02:56:45 +0900

    Add Customer Delete Module

--- client/src/components/Customer.jsx
+++ client/src/components/Customer.jsx
@@ -1,37 +1,21 @@
 import React from "react"
 import {TableCell,TableRow} from "@material-ui/core/"
+import CustomerDelete from "./CustomerDelete"
 
 class Customer extends React.Component{
     render(){
         return(
-                <TableRow>
+            <TableRow>
                 <TableCell>{this.props.id}</TableCell>
                 <TableCell><img src={this.props.image} alt="profile" style={{width:64, height:64}}/></TableCell>
                 <TableCell>{this.props.name}</TableCell>
                 <TableCell>{this.props.birthday}</TableCell>
                 <TableCell>{this.props.gender}</TableCell>
                 <TableCell>{this.props.job}</TableCell>
-                </TableRow>
+                <TableCell><CustomerDelete stateRefresh={this.props.stateRefresh} id={this.props.id}/></TableCell>
+            </TableRow>
         )
     }
 }
-class CustomerProfile extends React.Component{
-    render(){
-        return(
-            <div>
-                <img src={this.props.image} alt={this.props.id}/>
-            </div>
-        )
-    }
-}
-class CustomerInfo extends React.Component{
-    render(){
-        return(
-            <div>
-                <h2>이름:{this.props.name}</h2>
-                <h2>성별:{this.props.gender}</h2>
-            </div>
-        )
-    }
-}
+
 export default Customer
