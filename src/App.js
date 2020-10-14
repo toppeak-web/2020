5119a0619242f790d5b3cfc236bc4883e2ed3a36
toppeak <xkqvor123@gmail.com>
2020-10-14 21:53:18 +0900

    add Navbar materal Ui 네비게이션바 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Habits from './component/Habits';
+import Navbar from './component/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <Habits/>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    habits:[
+        {id:0, name:"Reading", count: 0},
+        {id:1, name:"Coding", count: 0},
+        {id:2, name:"Running", count: 0}
+
+    ]
+  }
+
+  handleIncrement = habit => {
+    // this.setState({count: this.state.count+1})
+    const habits = [...this.state.habits]
+    const index = habits.indexOf(habit) //this 배열의 순서
+    const count = habits[index].count++;  //해당 순서 index에 맞는 카운트를 1증가 시킨다
+    this.setState({ habits })
+    console.log(count)
+  }
+
+  handleDecrement = habit => {
+    const habits = [...this.state.habits]
+    const index = habits.indexOf(habit)
+    const count = habits[index].count - 1
+    habits[index].count = count < 0 ? 0 : count
+    this.setState({ habits })
+    console.log(count)
+  }
+
+  handleDelete = habit => {
+    const habits = this.state.habits.filter(i =>
+      i.id !== habit.id)
+    this.setState({ habits })
+  }
+
+  render() {
+    return (     
+      <>
+        <Navbar totalCount={this.state.habits.filter(i=> i.count > 0).length}/>   
+        <Habits
+           tomato={this.state.habits}
+           onIncrement={this.handleIncrement}
+           onDecrement={this.handleDecrement}
+           onDelete={this.handleDelete}
+           />   
+        
+      </>
+    );
+  }
 }
+
 
 export default App;
--- src/component/Habits.jsx
+++ src/component/Habits.jsx
@@ -2,42 +2,22 @@ import React, { Component } from 'react';
 import Habit from './Habit';
 
 class Habits extends Component {
-    state = {
-        habits:[
-            {id:0, name:"Reading", count: 0},
-            {id:1, name:"Coding", count: 0},
-            {id:2, name:"Running", count: 0}
-
-        ]
-    }
     
 
     handleIncrement = habit => {
-        // this.setState({count: this.state.count+1})
-        const habits = [...this.state.habits]
-        const index = habits.indexOf(habit) //this 배열의 순서
-        const count = habits[index].count++;  //해당 순서 index에 맞는 카운트를 1증가 시킨다
-        this.setState({habits})
-        console.log(count)
+        this.props.onIncrement(habit)
     }
     handleDecrement = habit =>{
-        const habits = [...this.state.habits]
-        const index = habits.indexOf(habit)
-        const count = habits[index].count-1
-        habits[index].count = count < 0 ? 0 : count
-        this.setState({habits})
-        console.log(count)
+        this.props.onDecrement(habit)
     }
 
     handleDelete = habit => {
-        const habits = this.state.habits.filter(i => 
-            i.id !== habit.id)
-            this.setState({ habits })
+        this.props.onDelete(habit)
     }
     render() {
         return (
            <ul>
-               {this.state.habits.map(habita=>(
+               {this.props.tomato.map(habita=>(
                    <Habit
                     key={habita.id}
                     ptato={habita}
