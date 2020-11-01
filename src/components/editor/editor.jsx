15cec0cced431dc03eb065495fb0cb3544692233
toppeak <xkqvor123@gmail.com>
2020-11-02 08:07:49 +0900

    5. add AddForm why error?

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import CardAdd from '../card_add/card_add';
 import CardEdit from '../card_edit/card_edit';
 import styles from "./editor.module.css"
 
-const Editor = ({cards}) => (
+const Editor = ({cards, addCard}) => (
            <section className={styles.editor}>
                <h1 className={styles.title}>Cark-Maker</h1>
                {cards.map(i=>(
-                   <CardEdit card={i}/>
+                   <CardEdit key={i.id} card={i}/>
                ))}
+               <CardAdd onAdd={addCard}/>
            </section>
     );
 
