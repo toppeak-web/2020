32087b8d4696abbf44f1f6cef54a1313352cb344
toppeak <xkqvor123@gmail.com>
2020-11-02 07:20:13 +0900

    4. add card and edit from component

--- src/components/card_edit/card_edit.jsx
+++ src/components/card_edit/card_edit.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React from "react";
+import styles from './card_edit.module.css'
 
-const CardEdit = ({card}) => (
-         <h1>{card.id}</h1>   
-    );
+const CardEdit = ({ card }) => {
+    const {
+        name,
+        company,
+        title,
+        email,
+        message,
+        theme,
+        fileName,
+        fileURL,
+      } = card;
+  return (
+      <form className={styles.form}>
+          <input className={styles.input} type="text" name="name" value={name}/>
+          <input className={styles.input} type="text" name="company" value={company}/>
+          <select className={styles.select} name="theme" value={theme}>
+              <option value="light">light</option>
+              <option value="dark">dark</option>
+              <option value="colorful">colorful</option>
+          </select>
+          <input className={styles.input} type="text" name="title" value={title}/>
+          <input className={styles.input} type="text" name="email" value={email}/>
+          <textarea name="message" value={message}/>
+          {/* button */}
+          <button className={styles.button}>
+              {name}
+          </button>
+      </form>
+  )
+};
 
 export default CardEdit;
